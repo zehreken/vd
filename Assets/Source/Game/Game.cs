0959b8e5ab2057c6b0617e1b2333281b6b525076
zehreken <zehreken@gmail.com>
2018-11-14 23:07:03 +0300

    Add Game

---
+++ Assets/Source/Game/Game.cs
@@ -0,0 +1,25 @@
+﻿namespace vd
+{
+	public class Game
+	{
+		public Game()
+		{
+			
+		}
+
+		public void StartGame()
+		{
+			
+		}
+
+		public void Update(float deltaTime)
+		{
+			
+		}
+
+		public void Destroy()
+		{
+			
+		}
+	}
+}
--- Assets/Source/Main.cs
+++ Assets/Source/Main.cs
@@ -5,9 +5,16 @@ namespace vd
 	public class Main : MonoBehaviour
 	{
 		public static Main Instance { get; private set; }
+		public Game _game;
+		public AppState State = AppState.Game;
 
 		void Start()
 		{
+			Instance = this;
+			Application.targetFrameRate = 60;
+			Input.multiTouchEnabled = false;
+			
+			Services.Initialize();
 		}
 
 		void Update()
