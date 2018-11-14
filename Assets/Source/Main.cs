7f75699beced4c34f8caf4a01bda2879b69240fc
zehreken <zehreken@gmail.com>
2018-11-14 22:59:04 +0300

    Add Main

---
+++ Assets/Source/Main.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace vd
+{
+	public class Main : MonoBehaviour
+	{
+		public static Main Instance { get; private set; }
+
+		void Start()
+		{
+		}
+
+		void Update()
+		{
+		}
+
+		public void Quit()
+		{
+			
+		}
+
+		public void Restart()
+		{
+			
+		}
+
+		public enum AppState
+		{
+			Game,
+			Pause
+		}
+
+		private void OnApplicationPause(bool isPaused)
+		{
+			if (isPaused)
+			{
+				// save game
+			}
+		}
+
+		private void OnApplicationQuit()
+		{
+			// save game
+		}
+	}
+}
