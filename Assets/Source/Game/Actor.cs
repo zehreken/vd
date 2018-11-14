6d4b71b3295b25a91374be247030f8c4846fa087
zehreken <zehreken@gmail.com>
2018-11-14 23:48:24 +0300

    Add Actor

---
+++ Assets/Source/Game/Actor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace vd
+{
+	public class Actor
+	{
+		public Actor()
+		{
+			
+		}
+
+		public void Update(float deltaTime)
+		{
+			
+		}
+	}
+
+	public class CollisionHelper : MonoBehaviour
+	{
+		private void OnTriggerEnter(Collider other)
+		{
+			Dbg.Log("collision");
+		}
+	}
+}
