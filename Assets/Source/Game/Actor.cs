cd952a86a35b4a3ad7cf40cda43423cd01a2fe48
zehreken <zehreken@gmail.com>
2018-11-16 01:43:50 +0300

    Add basic input

--- Assets/Source/Game/Actor.cs
+++ Assets/Source/Game/Actor.cs
@@ -6,6 +6,7 @@ namespace vd
 	{
 		private readonly GameObject _view;
 		private readonly Transform _transform;
+		private float _prevPosition;
 		
 		public Actor()
 		{
@@ -20,6 +21,17 @@ namespace vd
 //			_transform.Translate(Vector3.forward * 20f * deltaTime);
 //			if (_transform.localPosition.z > 100f)
 //				_transform.localPosition = new Vector3(0f, -2f, -100f);
+
+			if (Input.GetMouseButtonDown(0))
+			{
+				_prevPosition = Input.mousePosition.x;
+			}
+			else if (Input.GetMouseButton(0))
+			{
+				var fingerDelta = Input.mousePosition.x - _prevPosition;
+				_transform.RotateAround(Vector3.zero, Vector3.forward, fingerDelta * 20 * deltaTime);
+				_prevPosition = Input.mousePosition.x;
+			}
 		}
 
 		public Transform GetTransform()
