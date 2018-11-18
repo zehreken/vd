57d6188b897e4167629e62e2ca6ff3531c2963eb
zehreken <zehreken@gmail.com>
2018-11-18 18:06:41 +0300

    Add tags

--- Assets/Source/Game/Actor.cs
+++ Assets/Source/Game/Actor.cs
@@ -9,14 +9,16 @@ namespace vd
 		private readonly Transform _transform;
 		private readonly Transform _bodyTransform;
 		private float _prevPosition;
+		private ParticleSystem _particleSystem;
 		
 		public Actor()
 		{
 			_view = MiniPool.Create(PrefabName.Actor, new Vector3(0f, -2.25f, 0f));
 			_transform = _view.transform;
 			_bodyTransform = _transform.GetChild(0);
+			_particleSystem = _transform.GetChild(1).GetComponent<ParticleSystem>();
 
-			_view.AddComponent<CollisionHelper>();
+			_view.AddComponent<CollisionHelper>().Init(OnCollision);
 		}
 
 		public void Update(float deltaTime)
@@ -38,8 +40,15 @@ namespace vd
 			_bodyTransform.Rotate(Vector3.right, 1000f * deltaTime, Space.Self);
 		}
 
+		private void OnCollision()
+		{
+			_particleSystem.Stop();
+			MenuManager.Instance.Show(typeof(EndGameMenu));
+		}
+
 		public void Reset()
 		{
+			_particleSystem.Play();
 			_transform.localPosition = new Vector3(0f, -2.25f, 0f);
 			_bodyTransform.rotation = Quaternion.identity;
 		}
@@ -53,10 +62,20 @@ namespace vd
 
 	public class CollisionHelper : MonoBehaviour
 	{
+		private Action _onCollision;
+		
+		public void Init(Action onCollision)
+		{
+			_onCollision = onCollision;
+		}
+		
 		private void OnTriggerEnter(Collider other)
 		{
 			Dbg.Log("collision");
-			MenuManager.Instance.Show(typeof(EndGameMenu));
+			if (_onCollision != null)
+			{
+				_onCollision();
+			}
 		}
 	}
 }
